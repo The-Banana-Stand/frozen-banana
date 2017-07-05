49144367f2295583975ccf557b054edefcd7ef7b
Steven Thiesfeld <scthiesfeld@hotmail.com>
2017-07-05 11:38:22 -0500

    added cookie functionality, pending testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705144715) do
+ActiveRecord::Schema.define(version: 20170705160337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20170705144715) do
     t.text "dm_evaluating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
