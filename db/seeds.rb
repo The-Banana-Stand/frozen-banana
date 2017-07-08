cc4377403027c47c5bac0d3030c399038d15e38b
Steven Thiesfeld <scthiesfeld@hotmail.com>
2017-07-08 10:51:11 -0500

    added a few users to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+        first_name: 'Steven',
+        last_name: 'Thiesfeld',
+        username: 'StevenThiesfeld',
+        email: 'scthiesfeld@gmail.com',
+        company_name: 'The Banana Stand',
+        title: 'CTO',
+        company_address: '1009 Jones St.',
+        city: 'Omaha',
+        state: 'NE',
+        zip_code: '68102',
+        role: 'both',
+        phone_number: '4024900248',
+        password: 'password',
+        activated: true,
+        activated_at: Time.zone.now
+)
+
+User.create!(
+    first_name: 'Kyle',
+    last_name: 'Fowler',
+    username: 'KyleFowler',
+    email: 'kyleheinsonfowler@hotmail.com',
+    company_name: 'The Banana Stand',
+    title: 'CFO',
+    company_address: '1009 Jones St.',
+    city: 'Omaha',
+    state: 'NE',
+    zip_code: '68102',
+    role: 'both',
+    phone_number: '123456789',
+    password: 'password',
+    activated: true,
+    activated_at: Time.zone.now
+)
