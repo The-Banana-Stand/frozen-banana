777b2427ce1401ae67aae0ed7a761cc4fa866a1d
Steven Thiesfeld <scthiesfeld@hotmail.com>
2017-07-18 14:04:19 -0500

    fix to missing.png

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "60x60>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "60x60>" }, default_url: ":style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   has_secure_password
   has_paper_trail ignore: [:remember_digest]
