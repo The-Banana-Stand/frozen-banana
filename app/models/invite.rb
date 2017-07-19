9b4024970214c10cdace9f9fc12719b67363e15f
Steven Thiesfeld <scthiesfeld@hotmail.com>
2017-07-19 09:59:44 -0500

    changes to invite file upload, file and image now have size limits

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,7 +1,8 @@
 class Invite < ApplicationRecord
   has_paper_trail
   has_attached_file :attachment
-  validates_attachment_content_type :attachment, content_type: {content_type: %w(text/csv )}
+  validates_attachment_content_type :attachment, :content_type => %w(application/zip application/msword application/vnd.ms-office application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet text/csv text/plain)
+  validates_attachment_size :attachment, :in => 0.megabytes..2.megabytes
 
   belongs_to :user
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "60x60>" }, default_url: ":style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates_attachment_size :avatar, :in => 0.megabytes..4.megabytes
   has_secure_password
   has_paper_trail ignore: [:remember_digest]
   monetize :price_cents
