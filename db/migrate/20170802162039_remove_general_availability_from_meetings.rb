a3d80cfe54773130f655884773abc94d1a954314
Steven Thiesfeld <scthiesfeld@hotmail.com>
2017-08-02 12:14:56 -0500

    fixed error in migration

--- db/migrate/20170802162039_remove_general_availability_from_meetings.rb
+++ db/migrate/20170802162039_remove_general_availability_from_meetings.rb
@@ -5,7 +5,7 @@ class RemoveGeneralAvailabilityFromMeetings < ActiveRecord::Migration[5.1]
     add_column :meetings, :desired_end_time, :datetime
 
     Meeting.all.each do |meeting|
-      desired_block = meeting.desired_block
+      desired_block = GeneralAvailability.find(meeting.general_availability_id)
       meeting.desired_day = desired_block.day
       meeting.desired_start_time = desired_block.start_time
       meeting.desired_end_time = desired_block.end_time
