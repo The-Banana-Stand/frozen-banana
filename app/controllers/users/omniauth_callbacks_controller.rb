a2e4a34f3d6bc9965f821cef23ad14019ea94c48
Steven Thiesfeld <scthiesfeld@hotmail.com>
2017-08-05 09:02:32 -0500

    adding omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,6 @@
-class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
+  skip_before_action :authenticate_user!
 
   def linkedin
     # You need to implement the method below in your model (e.g. app/models/user.rb)
