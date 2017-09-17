367bc3d6290affe911be7ce3c7edd729dda10919
Steven Thiesfeld <scthiesfeld@hotmail.com>
2017-09-17 13:05:01 -0500

    using sendgrid in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -63,16 +63,15 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.default_url_options = { :host => 'localhost:3000' } #TODO change to real host, or more likely use mailing service
-  config.action_mailer.default_url_options = {host: 'https://staging.meetingslice.com'}
+  config.action_mailer.default_url_options = {host: 'https://meetingslice.com'}
   # SMTP settings for Amazon SES
   config.action_mailer.smtp_settings = {
-      :address        => 'smtp.office365.com',
+      :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :login,
-      :user_name      => ENV['SMTP_USERNAME'],
-      :password       => ENV['SMTP_PASSWORD'],
-      :domain         => 'staging.meetingslice.com',
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'meetingslice.com',
       :enable_starttls_auto => true
   }
 
