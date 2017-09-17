901800b0ec5738243ec77eaf1bdfa0030fc00ed1
Steven Thiesfeld <scthiesfeld@hotmail.com>
2017-09-17 13:53:26 -0500

    disabling emails in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "frozen-banana_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = {host: 'https://meetingslice.com'}
   # SMTP settings for Amazon SES
   config.action_mailer.smtp_settings = {
