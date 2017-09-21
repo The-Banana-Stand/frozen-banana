cbd5be4da5e3ef9a9770106c5cb639dd71a898ad
Steven Thiesfeld <scthiesfeld@hotmail.com>
2017-09-21 09:53:14 -0500

    finished wait time methods

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -8,12 +8,16 @@ class Bid < ApplicationRecord
 
   def place_in_line
     if self.persisted? && self.active?
-      Bid.where("price_cents <= ? AND meeting_queue_id = ? AND status = 0", self.price_cents, self.meeting_queue_id).count.ordinalize
+      rank.ordinalize
     elsif self.cancelled?
       'Bid Cancelled'
     else
       'No Bid'
     end
+  end
+
+  def rank
+    Bid.where("price_cents <= ? AND meeting_queue_id = ? AND status = 0", self.price_cents, self.meeting_queue_id).count
   end
 
   def wait_time
@@ -23,9 +27,28 @@ class Bid < ApplicationRecord
       return 'No Bid'
     end
 
-    time_available_unit = MeetingQueue.meeting_frequencies[self.meeting_queue.meeting_frequency]
+    rank_unit = (rank / 2).ceil
+    rank_unit -= 1 if rank.even?
+    weeks = (rank_unit * time_available_unit).floor
 
-
+    if weeks == 0
+      'On Meeting Selection Date'
+    elsif weeks == 1
+      weeks.to_s + ' week from meeting selection'
+    else
+      weeks.to_s + ' weeks from meeting selection'
+    end
 
   end
+
+  def time_available_unit
+    {
+        '1 hour per Week': 1,
+        '2 hours per Week': 0.5,
+        '1 hour every 2 weeks': 2,
+        '1 hour every 3 weeks': 3,
+        '1 hour every 4 weeks': 4
+    }.with_indifferent_access[self.meeting_queue.meeting_frequency]
+  end
+
 end
--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -20,10 +20,17 @@ RSpec.describe Bid, type: :model do
     it 'returns the estimated wait time of a bid' do
       queue = create(:meeting_queue)
       b1 = create(:bid, price_cents: 1000, meeting_queue: queue)
-      b2 = create(:bid, price_cents: 3000, meeting_queue: queue)
-      b3 = create(:bid, price_cents: 2000, meeting_queue: queue)
+      b2 = create(:bid, price_cents: 2000, meeting_queue: queue)
+      b3 = create(:bid, price_cents: 3000, meeting_queue: queue)
+      b4 = create(:bid, price_cents: 4000, meeting_queue: queue)
+      b5 = create(:bid, price_cents: 5000, meeting_queue: queue)
+      byebug
+      expect(b1.wait_time).to eq('On Meeting Selection Date')
+      expect(b2.wait_time).to eq('On Meeting Selection Date')
+      expect(b3.wait_time).to eq('4 weeks from meeting selection')
+      expect(b4.wait_time).to eq('4 weeks from meeting selection')
+      expect(b5.wait_time).to eq('8 weeks from meeting selection')
 
-      expect(b2.wait_time).to eq('4 weeks')
     end
   end
 end
